906186a1564b83cb23d526d71150a761041f25c7
MagAble <able4christ.aa@gmail.com>
2022-09-27 00:47:48 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,41 @@
+/**
+
+ *_memcpy - function that copies memory area
+
+ *
+
+ * @dest: pointer to the destination address of memory
+
+ *
+
+ * @src: pointer to source address of memry to be copied
+
+ *
+
+ *@n: non-negative number of bytes in memory area to be copied
+
+ *
+
+ * Return: pointer to destination address
+
+ */
+
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+
+	unsigned int i;
+
+
+
+	for (i = 0; i < n; i++)
+
+		dest[i] = src[i];
+
+
+
+	return (dest);
+
+}
