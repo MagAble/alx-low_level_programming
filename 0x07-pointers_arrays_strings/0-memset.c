8861be27a04216d6707b9e2470da759109e71d44
MagAble <able4christ.aa@gmail.com>
2022-09-27 00:44:56 -0700

    fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,53 @@
+/**
+
+ *_memset - function to fill memory with a constant byte
+
+ *
+
+ *@s: input pointer to char type to block of memory to be filled
+
+ *
+
+ *@b: input variable of char type to fill s
+
+ *
+
+ *@n: number of bytes to be filled
+
+ *
+
+ *Return: pointer to filled memory
+
+ */
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+	/**
+
+	 * declare an unsigned int
+
+	 * because we are storing a
+
+	 * value that will always be
+
+	 * non-negative (zero or positive)
+
+	*/
+
+	unsigned int i;
+
+
+
+	for (i = 0; i < n; i++)
+
+		s[i] = b;
+
+
+
+	return (s);
+
+}
