8706d82a749d00deadb42dbbfaa96e2f41764b9d
MagAble <able4christ.aa@gmail.com>
2022-09-27 23:51:22 -0700

    betty correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-  char ch;
-  int i = 10;
+char ch;
+int i = 10;
 
-  for (i =0; i < 10; i++)
-    {
-      ch = 'a';
-      while (ch <= 'z')
-	{
-	  _putchar(ch);
-	  ++ch;
-	}
-      _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+++ch;
+}
+_putchar('\n');
+}
 }
