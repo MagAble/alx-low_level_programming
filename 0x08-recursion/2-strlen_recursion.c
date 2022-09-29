c47710396d7d68ed59e1b84df041b9d55cc3ec41
MagAble <able4christ.aa@gmail.com>
2022-09-29 01:41:00 -0700

    length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+/**
+ *_strlen_recursion - function to find length of a string
+ *
+ * @s: pointer to string
+ *
+ *Return: len, length of the string
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+else
+return (0);
+}
